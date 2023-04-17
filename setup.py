62f185306e2054b6c358f3b2bcf8ed7e4393cca7
Kendall Holiday <kendall.holiday@inspirebrands.com>
2023-04-17 13:12:40 -0400

    gitlab_parse typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='gitlab_gpdb',
     version='0.1.0',
-    packages=['gitlab-parse'],
+    packages=['gitlab_parse'],
     url='',
     license='',
     author='kholiday',
