db4f465d44ca372709d30453b73e9cfb4b9af786
Kendall Holiday <kendall.holiday@inspirebrands.com>
2023-04-18 14:47:43 -0400

    fix parse test

--- gitlab_parse/push_parse.py
+++ gitlab_parse/push_parse.py
@@ -15,9 +15,35 @@ class PushData:
 def parse_push_data(push_data):
     push_data_obj = PushData(push_data)
 
+    gl_event = {
+        "project_id": push_data["project"]["id"],
+        "project_name": push_data["project"]["name"],
+        "ref": push_data["ref"],
+        "before_sha": push_data["before"],
+        "after_sha": push_data["after"],
+        "total_commits_count": push_data["total_commits_count"]
+    }
+
+    gl_commit_array = []
+
+    for commit in push_data["commits"]:
+        gl_commit = {
+            "commit_id": commit["id"],
+            "message": commit["message"],
+            "author_name": commit["author"]["name"],
+            "author_email": commit["author"]["email"],
+            "timestamp": commit["timestamp"],
+            "url": commit["url"],
+            "added": commit["added"],
+            "modified": commit["modified"],
+            "removed": commit["removed"],
+            "git_project_id": push_data["project"]["id"]
+        }
+        gl_commit_array.append(gl_commit)
 
     return {
         "gl_event": gl_event,
         "gl_commit_array": gl_commit_array,
         "result": "success"
     }
+
